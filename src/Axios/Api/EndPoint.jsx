91d8167ace18eb48fe0162ef297b042e680edba5
ashvn24 <ashwinashi6@gmail.com>
2024-03-22 13:16:47 +0530

    final updates

--- src/Axios/Api/EndPoint.jsx
+++ src/Axios/Api/EndPoint.jsx
@@ -1,6 +1,6 @@
-export const API = 'http://www.co-lab.website/api';
+export const API = 'https://www.co-lab.website/api';
 
-export const BASEURL = 'http://www.co-lab.website/';
+export const BASEURL = 'https://www.co-lab.website/';
 
 
 export const SOCKET = 'ws://127.0.0.1:8000/ws/chat';
